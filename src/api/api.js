8cbd20102dc83a3b70361d4336c5b0512e008f4b
Aliaksei Levashenia <mrfreeze987@gmail.com>
2023-11-18 22:55:52 +0300

    appwrite integration

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import { Client, Databases, ID, Query } from 'appwrite'
+
+const client = new Client()
+client
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject('6553a218b0966f5f5f85')
+
+const databases = new Databases(client)
+
+export const createNote = data => {
+  return databases.createDocument(
+    '6553c3b28d397388abdc',
+    '6558e325c72ed35a0b53',
+    ID.unique(),
+    data
+  )
+}
+
+export const getNotes = () => {
+  return databases.listDocuments('6553c3b28d397388abdc', '6558e325c72ed35a0b53')
+}
